8bfa2484bf42841d1918a8f755071d49471517e9
DarioSwede <darioswede@gmail.com>
2026-08-27 08:35:00 +0200

    Add UI module coordinator

---
+++ js/app-ui.js
@@ -0,0 +1,7 @@
+import {initUploadUi} from './upload-ui.js';
+import {initMaskMode} from './mask-mode.js';
+import {initDonePage} from './done-page.js';
+
+initUploadUi();
+initMaskMode();
+initDonePage();
