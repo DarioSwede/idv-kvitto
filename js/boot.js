b296d1e9996158af62160d5d1d0624b322c613cb
DarioSwede <darioswede@gmail.com>
2026-08-27 08:35:11 +0200

    Add modular app bootstrap

---
+++ js/boot.js
@@ -0,0 +1,19 @@
+import {applyTemplates} from './templates.js';
+
+async function boot(){
+  const response=await fetch('app-base.html?v=20260827-2',{cache:'no-store'});
+  if(!response.ok)throw new Error('Kunde inte ladda formuläret');
+  let html=await response.text();
+  html=applyTemplates(html);
+  html=html.replace('</head>','<link rel="stylesheet" href="styles/app.css?v=20260827-1"></head>');
+  html=html.replace('</body>','<script type="module" src="js/app-ui.js?v=20260827-1"><\/script></body>');
+  document.open();
+  document.write(html);
+  document.close();
+}
+
+boot().catch(err=>{
+  const bootEl=document.querySelector('.boot');
+  if(bootEl)bootEl.textContent='Kunde inte ladda formuläret. Ladda om sidan.';
+  console.error(err);
+});
