420f12375e5eaa8d8b79a49f7288a89c4a355971
Dario Swede <darioswede@gmail.com>
2026-08-27 21:25:08 +0200

    Refresh modular assets after deployment

--- js/boot.js
+++ js/boot.js
@@ -1,12 +1,12 @@
-import {applyTemplates} from './templates.js';
+import {applyTemplates} from './templates.js?v=20260827-3';
 
 async function boot(){
   const response=await fetch('app-base.html?v=20260827-2',{cache:'no-store'});
   if(!response.ok)throw new Error('Kunde inte ladda formuläret');
   let html=await response.text();
   html=applyTemplates(html);
-  html=html.replace('</head>','<link rel="stylesheet" href="styles/app.css?v=20260827-2"></head>');
-  html=html.replace('</body>','<script type="module" src="js/app-ui.js?v=20260827-2"><\/script></body>');
+  html=html.replace('</head>','<link rel="stylesheet" href="styles/app.css?v=20260827-3"></head>');
+  html=html.replace('</body>','<script type="module" src="js/app-ui.js?v=20260827-3"><\/script></body>');
   document.open();
   document.write(html);
   document.close();
