ed1929f23f15cad29fd7db336295ca5533e5880b
DarioSwede <darioswede@gmail.com>
2026-08-28 14:17:48 +0200

    Bump upload UI cache version

--- js/app-ui.js
+++ js/app-ui.js
@@ -1,4 +1,4 @@
-import {initUploadUi} from './upload-ui.js?v=20260828-2';
+import {initUploadUi} from './upload-ui.js?v=20260828-3';
 import {initMaskMode} from './mask-mode.js?v=20260828-1';
 import {initDonePage} from './done-page.js';
 import {initReceiptOcr} from './receipt-ocr.js?v=20260827-8';
